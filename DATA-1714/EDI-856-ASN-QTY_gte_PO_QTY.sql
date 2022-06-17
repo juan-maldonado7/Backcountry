b211cf8df8efb7d0beea02088bc298ab10fa2c85
juan_maldonado7 <juan.maldonado@backcountry.com>
2022-06-17 17:51:18 -0600

    assigned_to modify

--- DATA-1714/EDI-856-ASN-QTY_gte_PO_QTY.sql
+++ DATA-1714/EDI-856-ASN-QTY_gte_PO_QTY.sql
@@ -1,15 +1,16 @@
 with po_details as
 (
     select 
-        merchandise_division
+        merchandise_division, assigned_to_id, assigned_to
 	    ,count(distinct po_number) over (partition by merchandise_division ) as count_orders
 	    ,count(distinct vendor) over (partition by merchandise_division ) count_vendors
     from(
             select 
                 entity.name 								as vendor
                 ,po.tranid 									as po_number
-                ,merch.merchandise_division_name	        as merchandise_division
-                ,employ .full_name						    as buyer 
+                ,merch2.merchandise_division_name	        as merchandise_division
+                ,assigned.full_name     as assigned_to
+                ,merch2.assigned_to_id  
                 ,lines.transaction_line_id		            as line_id			
                 ,lines.item_count 						    as quantity_in_transaction_unit
             from 
@@ -25,15 +26,15 @@ with po_details as
             left join 
                 ns_entity.entity entity ON cast(po.entity_id as int64) = cast(entity.entity_id as int64)
             left join 
-                netsuite2.brand brand ON cast(brand.brand_id as int64) = cast(lines.brand_id as int64)
-            left join 
-                ns_sb1.merchandise_division merch ON cast(merch.merchandise_division_id as int64) = cast(po.merchandise_division_id as int64)
-            left join  
-                netsuite2.employees employ ON cast(po.sales_rep_id as int64) = cast(employ.employee_id as int64)
+                netsuite2.brand brand ON cast(brand.brand_id as int64) = cast(lines.brand_id as int64)            
+            left join
+              netsuite_sc.merchandise_division merch2 ON cast(merch2.merchandise_division_id as int64) = cast(po.merchandise_division_id as int64)
             left join 
                 netsuite.items_cv item ON cast(lines.item_id as int64) = cast(item.item_id as int64)
             left join
                 netsuite_sc.uom uom ON cast(item.uom_id as int64) = cast(uom.uom_id as int64)
+            left join
+                netsuite2.employees assigned ON assigned.employee_id=merch2.assigned_to_id
             where  
             po.transaction_type ='Purchase Order' and lines.asn_record is not null
             and lines.asn_quantity_shipped > lines.item_count 
@@ -43,9 +44,25 @@ with po_details as
     )
 )
 select 
-	merchandise_division 
+	 merchandise_division
+  ,assigned_to_id
+  ,assigned_to
 	,max(count_vendors) as count_vendors
 	,max(count_orders) as count_orders
 	,count(*) as count_problems_lines
 from po_details
-group by merchandise_division
+group by merchandise_division	, assigned_to_id, assigned_to 
+order by merchandise_division asc
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
